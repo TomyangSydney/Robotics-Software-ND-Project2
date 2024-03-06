d5d742411af0a915e60d48e9da044d08b23723b7
TomyangSydney <linchuanflanker@gmail.com>
2024-03-06 09:53:14 -0500

    Add repo to GitHub

---
+++ catkin_ws/build/ball_chaser/CMakeFiles/drive_bot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/mnt/d/Udacity_robotics_nanodegree/project2/catkin_ws/devel/lib/ball_chaser/drive_bot"
+  "/mnt/d/Udacity_robotics_nanodegree/project2/catkin_ws/devel/lib/ball_chaser/drive_bot.pdb"
+  "CMakeFiles/drive_bot.dir/src/drive_bot.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drive_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
